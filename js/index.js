068a9b11cfa5a09359682c563d83cb845cea5055
Mohamad Termanini <mohtermanini.job@gmail.com>
2022-03-31 21:24:27 +0300

    Add winning condition info based on grid size

--- js/index.js
+++ js/index.js
@@ -1,32 +1,40 @@
-
-
-import { game } from './game.js';
+import { game } from "./game.js";
+import { boardInformation } from "./Board/information.js";
 
 window.addEventListener("DOMContentLoaded", () => {
+    const gameOptionsForm = document.querySelector("#form-game-options");
+    const gameOptionsModal = document.querySelector("#modal-game-options");
+    const numberToPlaceSpan = gameOptionsModal.querySelector(".size-info");
 
-    let gameOptionsForm = document.querySelector("#form-game-options");
-    let gameOptionsModal = document.querySelector("#modal-game-options");
-
-    function attachEventsListeners(){
-        document.querySelectorAll('input[name="grid-size"]').forEach( item => {
-            item.addEventListener("change",labelChange); 
+    function attachEventsListeners() {
+        document.querySelectorAll('input[name="grid-size"]').forEach((item) => {
+            item.addEventListener("change", labelChange);
+            item.addEventListener("change", gridSizeChange);
         });
-        document.querySelectorAll('input[name="computerOrHuman"]').forEach( item => {
-            item.addEventListener("change" ,labelChange);
+        document.querySelectorAll('input[name="computerOrHuman"]').forEach((item) => {
+            item.addEventListener("change", labelChange);
         });
         gameOptionsForm.addEventListener("submit", startNewGame);
         document.querySelector(".btn-new-game").addEventListener("click", () => {
             openModal(gameOptionsModal);
-        })
-        document.querySelectorAll(".btn-modal-close").forEach( item => {
-            item.addEventListener("click", ()=>{
-                closeModal(document.querySelector(item.dataset['target']));
+        });
+        document.querySelectorAll(".btn-modal-close").forEach((item) => {
+            item.addEventListener("click", () => {
+                closeModal(document.querySelector(item.dataset["target"]));
             });
         });
         document.querySelector(".btn-restart-game").addEventListener("click", restartGame);
     }
 
-    function labelChange(e){
+    /* Grid Size */
+    function gridSizeChange(e) {
+        const newSize = parseInt(e.target.value, 10);
+        numberToPlaceSpan.textContent = boardInformation.getConsecutiveSymbolNumberToWin(newSize);
+    }
+
+    /* ==================== */
+
+    function labelChange(e) {
         let target = e.target;
         target.parentElement.parentElement.querySelectorAll("label").forEach((label) => {
             label.classList.remove("selected");
@@ -34,53 +42,51 @@ window.addEventListener("DOMContentLoaded", () => {
         target.parentElement.classList.add("selected");
     }
 
-    function startNewGame(e){
+    function startNewGame(e) {
         e.preventDefault();
         const form = e.target;
-        closeModal(form.closest(".modal"))
-        game.createGame(form['grid-size'].value, form['computerOrHuman'].value,
-            [form['player1-name'].value, form['player2-name'].value]
-        );
+        closeModal(form.closest(".modal"));
+        game.createGame(form["grid-size"].value, form["computerOrHuman"].value, [
+            form["player1-name"].value,
+            form["player2-name"].value,
+        ]);
     }
 
-    function restartGame(){
+    function restartGame() {
         game.restart();
     }
 
-    function closeModal(modalElement){
+    function closeModal(modalElement) {
         modalElement.classList.add("d-none");
-        document.body.classList.remove("active-modal")
+        document.body.classList.remove("active-modal");
     }
-    
-    function openModal(modalElement){
+
+    function openModal(modalElement) {
         modalElement.classList.remove("d-none");
         document.body.classList.add("active-modal");
     }
-    
-    document.querySelectorAll(".player-area form").forEach( (form) => {
+
+    document.querySelectorAll(".player-area form").forEach((form) => {
         form.addEventListener("submit", (e) => {
             e.preventDefault();
-            let player = game.getPlayerByIndex(form.dataset['player']);
+            let player = game.getPlayerByIndex(form.dataset["player"]);
             player.setName(form.name.value);
-            form.previousElementSibling.style['display'] = 'inline-block';
-            form.style['display'] = 'none';
+            form.previousElementSibling.style["display"] = "inline-block";
+            form.style["display"] = "none";
         });
     });
-    
-    document.querySelectorAll(".btn-change-name").forEach( item => {
+
+    document.querySelectorAll(".btn-change-name").forEach((item) => {
         item.addEventListener("click", () => {
             let form = item.nextElementSibling;
-            form.style['display'] = 'flex';
+            form.style["display"] = "flex";
             form.name.focus();
-            item.style['display'] = 'none';
-        })
+            item.style["display"] = "none";
+        });
     });
-    function loadApp(){
+    function loadApp() {
         openModal(gameOptionsModal);
         attachEventsListeners();
     }
     loadApp();
- })
-
-
-
+});
